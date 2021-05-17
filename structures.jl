44978bda5f086b541fdf81832d4d147e9ee7ccd0
Albert Lam <albert233@hotmail.com>
2021-05-16 19:16:41 -0500

    Added ConstantNode in structures.jl

--- structures.jl
+++ structures.jl
@@ -6,6 +6,12 @@ mutable struct VariableNode <: Node
     name::String
 end
 
+mutable struct ConstantNode <: Node
+    size::NTuple{N, Int} where {N}
+    eltype::Type
+    name::String
+end
+
 mutable struct Counter{T}
     node_type::T
     count::Int
@@ -17,5 +23,11 @@ function VariableNode(counter::Counter, size::NTuple{N, Int}, eltype::Type, name
     counter.count += 1
     name = isnothing(name) ? "var_$(counter.count)" : name
     return VariableNode(size, eltype, name)
+end
+
+function ConstantNode(counter::Counter, size::NTuple{N, Int}, eltype::Type, name::Union{Nothing, String}=nothing) where {N}
+    counter.count += 1
+    name = isnothing(name) ? "const_$(counter.count)" : name
+    return ConstantNode(size, eltype, name)
 end
 
